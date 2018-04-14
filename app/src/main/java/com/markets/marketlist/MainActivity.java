d1e449e8b565ac85eca341958f82f3319fcb2c3e
Kamil Kaleta <kamkalet@gmail.com>
2018-04-14 20:44:54 +0200

    initial commit

---
+++ app/src/main/java/com/markets/marketlist/MainActivity.java
@@ -0,0 +1,16 @@
+package com.markets.marketlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
